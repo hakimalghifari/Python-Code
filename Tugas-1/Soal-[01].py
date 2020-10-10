9af29129e646628f6769dcdf487157f1457ca3ed
hakimalghifari <hakim.alghifari@gmail.com>
2020-10-10 18:41:38 +0700

    Assignment#1

--- Tugas-1/Soal-[01].py
+++ Tugas-1/Soal-[01].py
@@ -1,6 +1,6 @@
 Nama = input("Masukkan nama anda ")
 Umur = input("Masukkan umur anda ")
-Tinggi = input("Masukkan tinggi anda ")
+Tinggi = float(input("Masukkan tinggi anda "))
 
-Biodata = "Nama saya {}, umur saya {} tahun dan tinggi saya {} cm". format(Nama, Umur, Tinggi)
+Biodata = "Nama saya {}, umur saya {} tahun dan tinggi saya {:.2f} cm". format(Nama, Umur, Tinggi)
 print(Biodata)
--- Tugas-1/Soal-[03].py
+++ Tugas-1/Soal-[03].py
@@ -1,14 +1,12 @@
-UjianTeori =input("Masukkan nilai ujian teori ")
-UjianPraktek = input("Masukkan nilai ujian praktek ")
+Uteori = float(input("Masukkan nilai ujian teori "))
+Upraktek = float(input("Masukkan nilai ujian praktek "))
 
-Nteori=float(UjianTeori)
-Npraktek=float(UjianPraktek)
 
-if Nteori>=70 and Npraktek>=70:
+if Uteori>=70 and Upraktek>=70:
     print("selamat, anda lulus!")
-elif Nteori>=70 and Npraktek<70:
+elif Uteori>=70 and Upraktek<70:
     print("Anda harus mengulang ujian praktek.")
-elif Nteori<70 and Npraktek>=70:
+elif Uteori<70 and Upraktek>=70:
     print("Anda harus mengulang ujian teori.")
-elif Nteori<70 and Npraktek<70:
+elif Uteori<70 and Upraktek<70:
     print("Anda harus mengulang ujian teori dan praktek.")
